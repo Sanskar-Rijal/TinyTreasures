15a5e8b0a8f224b28b438d036e364242e70478d5
Sanskar-Rijal <rijalsanskar@gmail.com>
2026-01-26 21:38:08 +0545

    cloudinary setup for images

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -1,6 +1,66 @@
 import AppError from "../utils/appError.js";
 import catchAsync from "../utils/catchAsync.js";
 import User from "../Models/user.js";
+import multer from "multer";
+import sharp from "sharp";
+import cloudinary from "../utils/cloudinary.js";
+//for image
+//1) Store file temporarily in memory
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+//2) Creating middleware to upload userPhoto
+const uploadUserPhoto = upload.single("photo");
+
+//3) Resize and upload user photo to cloudinary
+const resizeUserPhoto = catchAsync(async (req, res, next) => {
+  //upload.single("image") , to access files use "req.file"
+  //upload.array("images",5) , to access files use "req.files"
+  if (!req.file) return next();
+
+  //Resize the image using sharp before uploading to cloudinary
+  const buffer = await sharp(req.file.buffer)
+  .resize({width:500,height:500},{
+    fit:sharp.fit.cover, 
+    position:sharp.strategy.entropy
+  })
+  .toFormat("jpeg")
+  .jpeg({quality:90})
+  .toBuffer();
+
+ //now we will upload to cloudinary 
+ const stream = cloudinary.uploader.upload_stream(
+  {
+    folder:"avatars",
+    public_id:`user-${req.user.id}-${Date.now()}`,
+    resource_type:"image"
+  },(error,result)=>{
+    //Cloudinary lae basically k garxa vane ni haina image upload garxa ani hami lai chai ni,
+    //url dinxa k tyo image ko, ho tyo url chai hami database ma rakhna parne hunxa
+    if(error){
+      return next(new AppError("Error uploading image to Cloudinary",500));
+    }
+    //We will store the secure Url in req.file.cloudinaryAvatar
+    req.file.cloudinaryAvatar = result.secure_url;
+    req.file.public_id = result.public_id;
+    next();
+  }
+ ) 
+ //Sending the data to cloudinary via stream 
+ stream.end(buffer);
+});
+
 //get myself
 const getMe = catchAsync(async (req, res, next) => {
   //using cookie to get the current user from protect route
@@ -21,6 +81,15 @@ const updateProfile = catchAsync(async (req, res, next) => {
     email: req.body.email,
     name: req.body.name,
   };
+
+  //4)if user uploaded photo then we will add that to the updatedData
+  if(req.file && req.file.cloudinaryAvatar){
+    updatedData.avatar ={
+      public_id:req.file.public_id,
+      url:req.file.cloudinaryAvatar
+    };
+  }
+
   //update user data using patch request
   const updatedUser = await User.findByIdAndUpdate(userid, updatedData, {
     new: true,
@@ -82,14 +151,21 @@ const updateUser = catchAsync(async (req, res, next) => {
 const deleteUser = catchAsync(async (req, res, next) => {
   const userId = req.params.id;
   const deletedUser = await User.findByIdAndDelete(userId);
-  //delete avatar from cloudinary if implemented 
-  if(!deletedUser){
-    return next(new AppError("User not found with that id",404));
+  //delete avatar from cloudinary if implemented
+  if (!deletedUser) {
+    return next(new AppError("User not found with that id", 404));
   }
   res.status(204).json({
-    status:"true",
-    message:"Deleted successfully"
-  })
+    status: "true",
+    message: "Deleted successfully",
+  });
 });
 
-export { getMe, updateProfile, getAllUsers, getUserbyId, updateUser ,deleteUser};
+export {
+  getMe,
+  updateProfile,
+  getAllUsers,
+  getUserbyId,
+  updateUser,
+  deleteUser,
+};
